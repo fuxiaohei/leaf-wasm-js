edf931f51af109f599a16b8925a6940a8a0bd683
fuxiaohei <fuxiaohei@vip.qq.com>
2023-01-03 17:10:15 +0800

    feat: add quickjs-wasm-rs and wizer

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-.PHONY: build-release, build
+.PHONY: release, build
 
 build:
-	cargo build --target wasm32-wasi && \
+	cargo build --target wasm32-wasi
 	cp ./target/wasm32-wasi/debug/leaf-wasm-js.wasm ./
+	wizer leaf-wasm-js.wasm -o leaf-wasm-js-wizer.wasm --allow-wasi --inherit-stdio=true --inherit-env=true
 
-build-release:
-	cargo build --target wasm32-wasi --release && \
+release:
+	cargo build --target wasm32-wasi --release
 	cp ./target/wasm32-wasi/release/leaf-wasm-js.wasm ./
+	wizer leaf-wasm-js.wasm -o leaf-wasm-js-wizer.wasm --allow-wasi --inherit-stdio=true --inherit-env=true
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use quickjs_wasm_rs::Context;
+
+use std::io::{self, Read};
+
+#[export_name = "wizer.initialize"]
+pub extern "C" fn init() {
+    println!("wizer initialize");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_script = String::new();
+    io::stdin().read_to_string(&mut input_script).unwrap();
+
+    let context = Context::default();
+
+    let eval_result = context.eval_global("index.js", input_script.as_str());
+    if eval_result.is_err() {
+        println!("Error: {}", eval_result.err().unwrap());
+        return;
+    }
+    println!("eval_result: {}", eval_result.unwrap().as_str().unwrap());
+
+    println!("input_script: {}", input_script);
 }
