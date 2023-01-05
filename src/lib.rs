52acfdd0eeceb9462ce32d94d3d0d53e5e72ee18
Gitea <gitea@fake.local>
2023-01-05 22:49:16 +0800

    feat: add quickjs in http handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use bytes::Bytes;
 use leaf_sdk::http::{Request, Response};
 use leaf_sdk_macro::http_main;
+use quickjs_wasm_rs::Context;
 use std::str::FromStr;
 
 #[export_name = "wizer.initialize"]
@@ -9,14 +11,26 @@ pub extern "C" fn init() {
 
 #[http_main]
 pub fn handle_sdk_http(req: Request) -> Response {
-    let url = req.uri().clone();
-    let method = req.method().to_string().to_uppercase();
-    let body = req.into_body();
+    handle_request(req)
+}
+
+pub fn handle_request(_req: Request) -> Response {
+    let input_script = String::from("let a = 1;a + 2");
+
+    let context = Context::default();
+    let eval_result = context.eval_global("index.js", input_script.as_str());
+
+    if eval_result.is_err() {
+        return http::Response::builder()
+            .status(500)
+            .body(Some(Bytes::from(eval_result.err().unwrap().to_string())))
+            .unwrap();
+    }
+
+    let body = eval_result.unwrap().as_str().unwrap().to_string();
     let resp = http::Response::builder()
-        .header("X-Request-Url", url.to_string())
-        .header("X-Request-Method", method)
         .status(200)
-        .body(body)
+        .body(Some(Bytes::from(body)))
         .unwrap();
     resp
 }
