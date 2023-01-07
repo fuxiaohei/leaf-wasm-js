cd3bc5ec6606755df41e22a4a56103cbe16d8acc
Gitea <gitea@fake.local>
2023-01-07 23:34:55 +0800

    feat: use wizer initialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
+use anyhow::Result;
 use bytes::Bytes;
 use leaf_sdk::http::{Request, Response};
 use leaf_sdk_macro::http_main;
+use once_cell::sync::OnceCell;
 use quickjs_wasm_rs::Context;
+use send_wrapper::SendWrapper;
 use std::str::FromStr;
+
+static JS_CONTEXT: OnceCell<SendWrapper<Context>> = OnceCell::new();
+
+fn init_js_context() -> Result<()> {
+    let context = Context::default();
+    JS_CONTEXT.set(SendWrapper::new(context)).unwrap();
+    Ok(())
+}
 
 #[export_name = "wizer.initialize"]
 pub extern "C" fn init() {
-    println!("wizer initialize");
+    println!("js context initialize");
+    init_js_context().unwrap();
 }
 
 #[http_main]
@@ -17,7 +29,7 @@ pub fn handle_sdk_http(req: Request) -> Response {
 pub fn handle_request(_req: Request) -> Response {
     let input_script = String::from("let a = 1;a + 2");
 
-    let context = Context::default();
+    let context = JS_CONTEXT.get().unwrap();
     let eval_result = context.eval_global("index.js", input_script.as_str());
 
     if eval_result.is_err() {
